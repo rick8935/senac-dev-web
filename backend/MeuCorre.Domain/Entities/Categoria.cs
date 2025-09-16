3864bf7457dd4b8cb3a3f71f42f75aa2ac0a2e48
rick8935 <senac.rickboone@gmail.com>
2025-09-15 21:53:03 -0300

    feat(backend): #11 Adiciona entidade de categoria

--- backend/MeuCorre.Domain/Entities/Categoria.cs
+++ backend/MeuCorre.Domain/Entities/Categoria.cs
@@ -2,32 +2,56 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace MeuCorre.Domain.Entities
 {
     public class Categoria : Entidade 
     {
         public string Nome { get; private set; }
-        public string Descricao { get; private set; }
+        public string? Descricao { get; private set; }
         public TipoTransacao TipoTransacao { get; private set; }
-        public string Cor { get; private set; }
-        public string Icone { get; private set; }
+        public string? Cor { get; private set; }
+        public string? Icone { get; private set; }
         public Guid? UsuarioId { get; private set; }
         public bool Ativo { get; private set; }
 
         public virtual Usuario Usuario { get; private set; }
 
-        public Categoria(string nome, string descricao, TipoTransacao tipoTransacao, string cor, string icone, Guid? usuarioId, bool ativo)
+        public Categoria(string nome, string? descricao, TipoTransacao tipoTransacao, string? cor, string? icone, Guid? usuarioId, bool ativo)
         {
-            Nome = nome;
+            Nome = nome.ToUpper();
             Descricao = descricao;
             Cor = cor;
             Icone = icone;
             UsuarioId = usuarioId;
-            Ativo = ativo;
+            Ativo = true;
             TipoTransacao = tipoTransacao;
+        }
+
+        public void AtualizarInformacoes(string nome, string? descricao, string? cor, string? icone, TipoTransacao tipoTransacao)
+        {
+            Nome = nome.ToUpper();
+            Descricao = descricao;
+            Cor = cor;
+            Icone = icone;;
+            TipoTransacao = tipoTransacao;
+            AtualizarDataMoficacao();
+        }
+
+        public void Ativar()
+        {
+            Ativo = true;
+            AtualizarDataMoficacao();
+        }
+
+        public void Inativar()
+        {
+            Ativo = false;
+            AtualizarDataMoficacao();
         }
     }
 }
