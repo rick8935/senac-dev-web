5339ea32d21a882ed2f8cf53f208ef4401e372f4
rick8935 <senac.rickboone@gmail.com>
2025-10-02 19:58:08 -0300

    feat(backend): Implementa Query ObterConta

--- backend/MeuCorre.Application/UserCases/Contas/Queries/ObterContaQuery.cs
+++ backend/MeuCorre.Application/UserCases/Contas/Queries/ObterContaQuery.cs
@@ -1,1 +1,64 @@
-﻿
+﻿using MediatR;
+using MeuCorre.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Application.UserCases.Contas.Queries
+{
+    public class ObterContaQuery : IRequest<ContaDetalheResponse> // Added IRequest<ContaDetalheResponse> implementation
+    {
+        public required Guid UsuarioId { get; set; }
+        public required Guid ContaId { get; set; }
+    }
+
+    public class ContaDetalheResponse
+    {
+        public required Guid UsuarioId { get; set; }
+        public required Guid ContaId { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public decimal Saldo { get; set; }
+
+        public int QuantidadeTransacoes { get; set; }
+        public decimal TotalReceitas { get; set; }
+        public decimal TotalDespesas { get; set; }
+    }
+
+    internal class ObterContaQueryHandler : IRequestHandler<ObterContaQuery, ContaDetalheResponse>
+    {
+        private readonly IContaRepository _contaRepository;
+
+        public ObterContaQueryHandler(IContaRepository contaRepository)
+        {
+            _contaRepository = contaRepository;
+        }
+
+        public async Task<ContaDetalheResponse> Handle(ObterContaQuery request, CancellationToken cancellationToken)
+        {
+            var conta = await _contaRepository.ObterPorIdEUsuarioAsync(request.ContaId, request.UsuarioId);
+            if (conta == null)
+            {
+                throw new KeyNotFoundException("Conta não encontrada.");
+            }
+
+            int quantidade = 0;
+            int receitas = 0;
+            int despesas = 0;
+
+            return new ContaDetalheResponse
+            {
+                ContaId = conta.Id,
+                UsuarioId = conta.UsuarioId,
+                Nome = conta.Nome,
+                Tipo = conta.Tipo.ToString(),
+                Saldo = conta.Saldo,
+                QuantidadeTransacoes = quantidade,
+                TotalReceitas = receitas,
+                TotalDespesas = despesas
+            };
+        }
+    }
+}
